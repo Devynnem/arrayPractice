8d04688a0fb0be6490bd67d41c465fb89ea97c5c
Devynne Marshall <devynnem@gmail.com>
2022-12-07 18:14:40 -0700

    Show my understanding of array methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,12 @@
 var students = ["Blake", "Vicky", "Harry", "Ty"];
 var temperature = [34, 42, 55, 28];
 var cold = [true, true, false, true];
+// pop will remove the last element in my students array
+students.pop()
+console.log(students)
+// shift will remove first element in my temperature array
+temperature.shift()
+console.log(temperature)
+// push will add another false element to my cold array
+cold.push(false)
+console.log(cold)
